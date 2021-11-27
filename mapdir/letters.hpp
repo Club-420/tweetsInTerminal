54d13de5067d037cc1ceac17d06776e4c5d9fd9d
ImPrajwalAcharya <prajwalacp222@gmail.com>
2021-11-27 20:12:36 +0545

    modified2021

--- mapdir/letters.hpp
+++ mapdir/letters.hpp
@@ -21,15 +21,191 @@ const string a=
 const string b=
 "┌┐ \n"
 "├┴┐\n"
-"└─┘\n"
+"└─┘\n";
 
 const string c=
 
 "┌─┐\n"
 "│  \n"
-"└─┘\n"
+"└─┘\n";
 
 const string d=
 "┌┬┐\n"
 " ││\n"
-"─┴┘\n"
+"─┴┘\n";
+
+
+const string e=
+
+"┌─┐\n"
+"├┤ \n"
+"└─┘\n";
+
+
+const string f=
+
+"┌─┐\n"
+"├┤ \n"
+"└  \n";
+
+
+const string g=
+
+
+"┌─┐\n"
+"│ ┬\n"
+"└─┘\n";
+
+
+const string h=
+
+
+"┬ ┬\n"
+"├─┤\n"
+"┴ ┴\n";
+
+
+const string i=
+
+
+"┬\n"
+"│\n"
+"┴\n";
+
+
+const string j=
+
+
+" ┬\n"
+" │\n"
+"└┘\n";
+
+
+const string k=
+
+
+"┬┌─\n"
+"├┴┐\n"
+"┴ ┴\n";
+
+
+const string l=
+
+
+"┬  \n"
+"│  \n"
+"┴─┘\n";
+
+
+const string m=
+
+
+"┌┬┐\n"
+"│││\n"
+"┴ ┴\n";
+
+
+const string n=
+
+
+"┌┐┌\n"
+"│││\n"
+"┘└┘\n";
+
+
+const string o=
+
+
+"┌─┐\n"
+"│ │\n"
+"└─┘\n";
+
+
+const string p=
+
+
+"┌─┐\n"
+"├─┘\n"
+"┴  \n";
+
+
+const string q=
+
+
+"┌─┐ \n"
+"│─┼┐\n"
+"└─┘└\n";
+
+
+const string r=
+
+
+"┬─┐\n"
+"├┬┘\n"
+"┴└─\n";
+
+
+const string s=
+
+
+"┌─┐\n"
+"└─┐\n"
+"└─┘\n";
+
+
+const string t=
+
+
+"┌┬┐\n"
+" │ \n"
+" ┴ \n";
+
+
+const string u=
+
+
+"┬ ┬\n"
+"│ │\n"
+"└─┘\n";;
+
+
+const string v=
+
+
+"┬  ┬\n"
+"└┐┌┘\n"
+ "└┘ \n";
+
+
+const string w=
+
+
+"┬ ┬\n"
+"│││\n"
+"└┴┘\n";
+
+
+const string x=
+
+
+
+"─┐ ┬\n"
+"┌┴┬┘\n"
+"┴ └─\n";
+
+
+
+
+const string y=
+
+
+"┬ ┬\n"
+"└┬┘\n"
+ "┴ \n";
+
+const string z=
+
+"┌─┐\n"
+"┌─┘\n"
+"└─┘\n";
+
